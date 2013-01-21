771e247d74214d30798fa411df25f921532ef0b5
jdifranco <jdifranco@arg-jdifranco.deremate.bue>
2013-01-21 10:40:50 -0300

    Initial Upload

---
+++ transactions/src/main/java/asd/asd/App.java
@@ -0,0 +1,13 @@
+package asd.asd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
